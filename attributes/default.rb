fd4de1b6ee4994b6b3fb2d9d5c55bdf553fabcbd
Jay Flowers <jay.flowers@gmail.com>
2018-01-22 10:32:32 -0500

    commit for version : 1.0.4

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,17 +3,7 @@ default[:yum_mirror][:disable_others] = false
 default[:yum_mirror][:fail_for_others] = false
 default[:yum_mirror][:favor] = false
 
-default[:yum_mirror][:mirrors] = [
-	{
-		name: 'mirror',
-		volume_mount_path: '/var/lib/mirror',
-		url:  :fqdn, # or a string url, the symbol :fqdn will be replaced with a url constructed from the fqdn: https://#{fqdn}/
-		volume_size: 500,
-		volume_iops: 500,
-		volume_device: '/dev/sdj',
-		volume_mount_point: '/dev/xvdn'
-	}
-]
+default[:yum_mirror][:mirrors] = []
 default[:yum_mirror][:basedir] = '/var/lib/mirror'
 default[:yum_mirror][:repodir] = "#{node[:yum_mirror][:basedir]}/repo"
 default[:yum_mirror][:repo][:apache][:customizations] = ""
@@ -48,4 +38,3 @@ default[:apache][:ssl_cert_file]        = "#{node[:apache][:ssl_dir]}/server.crt
 default[:apache][:ssl_cert_key_file]    = "#{node[:apache][:ssl_dir]}/server.key"
 default[:apache][:ssl_cert_chain_file]  = "#{node[:apache][:ssl_dir]}/chain.crt"
 default[:apache][:root_dir]             = '/var/www'
-
--- recipes/aws.rb
+++ recipes/aws.rb
@@ -13,21 +13,17 @@ if in_ec2?()
     action :associate
   end
 
-  node[:yum_mirror][:mirrors].each{|repo_info|
-
-    next if repo_info[:volume_size].nil?
-
-    ec2_ebs_volume repo_info[:name] do
-      aws_access_key aws[:key]
-      aws_secret_access_key aws[:secret]
-      volume_size repo_info[:volume_size]
-      volume_type 'io1'
-      volume_iops repo_info[:volume_iops]
-      volume_mount_path repo_info[:volume_mount_path]
-      volume_device repo_info[:volume_device]
-      volume_mount_point repo_info[:volume_mount_point]
-    end
-  }
+  ec2_ebs_volume 'yum_secondary_drive' do
+    aws_access_key aws[:key]
+    aws_secret_access_key aws[:secret]
+    volume_size 500
+    volume_type "io1"
+    volume_iops 500
+    volume_mount_path '/var/lib/mirror'
+    volume_device '/dev/sdi'
+    volume_mount_point '/dev/xvdi'
+    fstype :btrfs
+  end
 end
 
 include_recipe 'ec2'
